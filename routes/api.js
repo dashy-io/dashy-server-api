d12cd8eeeaa9e25a74905c7149381440ca4088c3
Marco Bettiolo <marco@bettiolo.it>
2014-11-26 12:22:50 +0000

    Added status endpoint

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var router = express.Router();
+var app = express();
+
+router.get('/status', function(req, res) {
+  res.status(200);
+  res.json({
+    env: app.get('env')
+  });
+});
+
+module.exports = router;
