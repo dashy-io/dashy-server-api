7c417d0cf383bc8de60af2205da239f5ad6e2f6f
Marco Bettiolo <marco@bettiolo.it>
2014-11-27 01:47:09 +0000

    Added dashboard names and stub user handling

--- routes/api.js
+++ routes/api.js
@@ -13,10 +13,11 @@ router.get('/status', function(req, res) {
 router.get('/dashboards/:id', function(req, res, next) {
   var id = req.params.id;
   var dashboard = {
-    interval: 60
+    interval : 60
   };
   switch (id) {
     case 'test':
+      dashboard.name = 'Test Dashboard';
       dashboard.interval = 15;
       dashboard.urls = [
         'http://citydashboard.org/london/',
@@ -26,6 +27,7 @@ router.get('/dashboards/:id', function(req, res, next) {
       ];
       break;
     case 'e6e02fd1-6ed3-45d3-b1a7-de4fe3d32906':
+      dashboard.name = 'TechHub';
       dashboard.urls = [
         'http://s3-eu-west-1.amazonaws.com/mattia.test/community-messages/index.html',
         'http://s3-eu-west-1.amazonaws.com/mattia.test/flex_desks/1FlexShow.html'
@@ -39,6 +41,34 @@ router.get('/dashboards/:id', function(req, res, next) {
   }
   res.status(200);
   res.json(dashboard);
+});
+
+router.get('/user/:id', function(req, res, next) {
+  var id = req.params.id;
+  var user = {
+    id : id
+  };
+  switch (id) {
+    case 'test-user':
+      user.name = 'Test User';
+      user.dashboards = [
+        'test'
+      ];
+      break;
+    case 'cc1f2ba3-1a19-44f2-ae78-dc9784a2a60f':
+      user.name = 'TechHub User';
+      user.dashboards = [
+        'e6e02fd1-6ed3-45d3-b1a7-de4fe3d32906'
+      ];
+      break;
+    default:
+      var notFoundError = new Error('User Not Found');
+      notFoundError.status = 404;
+      next(notFoundError);
+      return;
+  }
+  res.status(200);
+  res.json(user);
 });
 
 module.exports = router;
