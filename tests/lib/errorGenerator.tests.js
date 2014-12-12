f30486cf6692afc69dd3d0462a9e64d7d109a58e
Marco Bettiolo <marco@bettiolo.it>
2014-12-12 01:14:05 +0000

    created error generator

---
+++ tests/lib/errorGenerator.tests.js
@@ -0,0 +1,18 @@
+'use strict';
+var chai = require('chai');
+var chaiString = require('chai-string');
+var errorGenerator = require('../../lib/errorGenerator');
+var assert = chai.assert;
+chai.use(chaiString);
+
+describe('errorGenerator', function () {
+  it('generates error with custom status code and message', function () {
+    var error = errorGenerator.create(500, 'custom error');
+    assert.equal(error.status, 500);
+  });
+  it('generates 400 Bad Request for missing property', function () {
+    var error = errorGenerator.createPropertyMissing('propertyName');
+    assert.equal(error.status, 400);
+    assert.equal(error.message, 'Property "propertyName" missing in body');
+  });
+});
