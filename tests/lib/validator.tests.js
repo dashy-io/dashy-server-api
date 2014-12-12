aed44602849e31b2878fc96b2319f23165fa10a8
Marco Bettiolo <marco@bettiolo.it>
2014-12-12 01:39:48 +0000

    Added checking for unexpected properties and compacted handling of required properties

--- tests/lib/validator.tests.js
+++ tests/lib/validator.tests.js
@@ -28,4 +28,40 @@ describe('Validating required properties', function () {
     var validationResult = validator.requireProperties({ prop : 'value' }, ['prop']);
     assert.isNull(validationResult);
   });
+  it('returns first missing property if more than one missing', function () {
+    var validationResult = validator.requireProperties({ prop : 'value' }, ['property1', 'property2']);
+    assert.isNotNull(validationResult);
+    assert.equal(validationResult.missingProperty, 'property1')
+  });
+});
+
+describe('Validating allowed properties', function () {
+  it('handles no allowed properties', function () {
+    var validationResult = validator.allowProperties({});
+    assert.isNull(validationResult);
+  });
+  it('handles no body', function () {
+    var validationResult = validator.allowProperties();
+    assert.isNull(validationResult);
+  });
+  it('validates allowed property on a null body', function () {
+    var validationResult = validator.allowProperties(null, ['property1']);
+    assert.isNull(validationResult);
+  });
+  it('returns unexpected property', function () {
+    var validationResult = validator.allowProperties({ unexpected : 'value' }, ['property1']);
+    assert.isNotNull(validationResult);
+    assert.equal(validationResult.unexpectedProperty, 'unexpected')
+  });
+  it('validates property', function () {
+    var validationResult = validator.allowProperties({ prop : 'value' }, ['prop']);
+    assert.isNull(validationResult);
+  });
+  it('returns first unexpected property if more than one unexpected', function () {
+    var validationResult = validator.allowProperties(
+      { unexpected1 : 'value', unexpected2 : 'value' }, ['property1']);
+    assert.isNotNull(validationResult);
+    assert.equal(validationResult.unexpectedProperty, 'unexpected1')
+  });
+
 });
