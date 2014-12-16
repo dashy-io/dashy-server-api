fe7b3f75d30e35f3ae4fe5ad4af6c131d88a0b0a
Marco Bettiolo <marco@bettiolo.it>
2014-12-16 03:03:54 +0000

    Added full user journey

--- tests/user-journey.tests.js
+++ tests/user-journey.tests.js
@@ -1,30 +1,93 @@
-//describe('Completes a full user journey', function (done) {
-//  // create a dashboard like a raspberry pi
-//  request.post('/dashboards')
-//    .send({ id: uuid.v4() })
-//    .expect(201)
-//    .end(function(err, res) {
-//      if (err) { return done(err); }
-//      var dashboard = res.body;
-//      // register a user
-//      request.post('/user')
-//        .send({ email: 'user@example.com' })
-//        .expect(201)
-//        .end(function (err, res) {
-//          if (err) { return done(err); }
-//          // get dashboard code
-//          request.get('/dashboards/' + dashboard.id + '/code')
-//            .expect(200)
-//            .end(function (err, res) {
-//              if (err) { return done(err); }
-//              // connect a dashboard to user
-//              // get user's dashboards
-//              // edit a dashboard
-//              // get a dashboard
-//              // delete a dashboard
-//              // delete a user
-//              done();
-//            });
-//        });
-//    });
-//});
+'use strict';
+var uuid = require('node-uuid');
+var request = require('supertest');
+var chai = require('chai');
+var chaiString = require('chai-string');
+var app = require('../app');
+// var testHelpers = require('./test-helpers');
+var assert = chai.assert;
+request = request(app);
+chai.use(chaiString);
+
+describe('User Journey', function () {
+  this.timeout(20000);
+  it('Completes a full user journey', function (done) {
+    // create a dashboard like a raspberry pi
+    request.post('/dashboards')
+      .send({id: uuid.v4()})
+      .expect(201)
+      .end(function (err, res) {
+        if (err) { return done(err); }
+        var dashboard = res.body;
+        // register a user
+        request.post('/users')
+          .send({ email: 'user@example.com', name : 'Test user', dashboards : [] })
+          .expect(201)
+          .end(function (err, res) {
+            if (err) { return done(err); }
+            var user = res.body;
+            // get dashboard code
+            request.get('/dashboards/' + dashboard.id + '/code')
+              .expect(200)
+              .end(function (err, res) {
+                if (err) { return done(err); }
+                var dashboardCode = res.body.code;
+                // connect a dashboard to user
+                request.post('/users/' + user.id + '/dashboards')
+                  .send({code: dashboardCode})
+                  .expect(201)
+                  .end(function (err, res) {
+                    if (err) { return done(err); }
+                    // get user's dashboards
+                    request.get('/users/' + user.id)
+                      .expect(200)
+                      .end(function (err, res) {
+                        if (err) { return done(err); }
+                        user = res.body;
+                        assert.deepEqual(user.dashboards, [ dashboard.id ]);
+                        dashboard.name = 'Test dashboard for user journey';
+                        // edit a dashboard
+                        request.put('/dashboards/' + dashboard.id)
+                          .send(dashboard)
+                          .expect(200)
+                          .expect(dashboard)
+                          .end(function (err, res) {
+                            if (err) { return done(err); }
+                            // get a dashboard
+                            request.get('/dashboards/' + dashboard.id)
+                              .expect(200)
+                              .expect(dashboard)
+                              .end(function (err, res) {
+                                if (err) { return done(err); }
+                                // delete a dashboard
+                                request.delete('/dashboards/' + dashboard.id)
+                                  .expect(204)
+                                  .end(function (err, res) {
+                                    if (err) { return done(err); }
+                                    request.get('/dashboards/' + dashboard.id)
+                                      .expect(404)
+                                      .end(function (err, res) {
+                                        if (err) { return done(err); }
+                                        // delete a user
+                                        request.delete('/users/' + user.id)
+                                          .expect(204)
+                                          .end(function (err, res) {
+                                            if (err) { return done(err); }
+                                            request.get('/users/' + user.id)
+                                              .expect(404)
+                                              .end(function (err, res) {
+                                                if (err) { return done(err); }
+                                                done();
+                                              });
+                                          });
+                                      });
+                                  });
+                              });
+                          });
+                      });
+                  });
+              });
+          });
+      });
+  });
+});
