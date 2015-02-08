d3a367200f8f014f3b8ccfe2a1e6eab6269ad052
Marco Bettiolo <marco@bettiolo.it>
2015-02-08 18:22:57 +0000

    cleanup

--- endpoints/users.js
+++ endpoints/users.js
@@ -12,28 +12,6 @@ function clone(input) {
   return JSON.parse(JSON.stringify(input));
 }
 
-//router.post('/users', function (req, res, next) {
-//  var user = clone(req.body);
-//  var requiredProperties = ['email', 'name', 'dashboards'];
-//  var validationError = validator.requireProperties(user, requiredProperties);
-//  if (validationError) {
-//    return next(errorGenerator.missingProperty(validationError.missingProperty));
-//  }
-//  validationError = validator.allowProperties(user, requiredProperties);
-//  if (validationError) {
-//    return next(errorGenerator.unexpectedProperty(validationError.unexpectedProperty));
-//  }
-//  user.id = 'user-' + uuid.v4();
-//  DataStore.create(function (err, dataStore) {
-//    if (err) { return next(err); }
-//    dataStore.createUser(user, function (err, createdUser) {
-//      if (err) { return next(err); }
-//      res.status(201);
-//      res.json(createdUser);
-//    });
-//  });
-//});
-
 router.get('/users/:id?', function (req, res, next) {
   var id = req.params.id;
   if (!id) {
@@ -69,52 +47,12 @@ router.get('/user', function (req, res, next) {
         if (!user) {
           return next(errorGenerator.notFound('User associated with token'));
         }
-        // user.emails = user.linkedProfiles.google.emails.map(function (email) { return email.value });
-        // user.name = user.linkedProfiles.google.displayName || user.emails[0];
-        // user.imageUrl = user.linkedProfiles.google.image.url.replace('?sz=50', '');
-        // user.domain = user.linkedProfiles.google.domain;
         res.status(200);
         res.json(user);
       });
     });
   });
 });
-
-//router.put('/users/:id?', function (req, res, next) {
-//  var id = req.params.id;
-//  var user = clone(req.body);
-//  var requiredProperties = ['email', 'name', 'dashboards'];
-//  var allowedProperties = ['id', 'email', 'name', 'dashboards'];
-//  if (!id) {
-//    return next(errorGenerator.missingParameter('id'));
-//  }
-//  var validationError = validator.allowMatchingId(user, id);
-//  if (validationError) {
-//    return next(errorGenerator.notMatchingProperty(validationError.notMatchingProperty));
-//  }
-//  validationError = validator.requireProperties(user, requiredProperties);
-//  if (validationError) {
-//    return next(errorGenerator.missingProperty(validationError.missingProperty));
-//  }
-//  validationError = validator.allowProperties(user, allowedProperties);
-//  if (validationError) {
-//    return next(errorGenerator.unexpectedProperty(validationError.unexpectedProperty));
-//  }
-//  DataStore.create(function (err, dataStore) {
-//    if (err) { return next(err); }
-//    dataStore.getUser(id, function (err, existingUser) {
-//      if (err) { return next(err); }
-//      if (!existingUser) {
-//        return next(errorGenerator.notFound('User'));
-//      }
-//      dataStore.updateUser(id, user, function(err, updatedUser) {
-//        if (err) { return next(err); }
-//        res.status(200);
-//        res.json(updatedUser);
-//      });
-//    });
-//  });
-//});
 
 router.delete('/users/:id?', function (req, res, next) {
   var id = req.params.id;
